6fbc068bd18526570347ede808a2a0dad2da0211
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-11 02:46:34 +0800

    kita rehat sejenak

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,10 @@
-const Heading = ({ className, tittle }) => {
+const Heading = ({ className, tittle, text }) => {
   return (
     <div
       className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
     >
       {tittle && <h2 className="h2">{tittle}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 };
