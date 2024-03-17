17b2176fb65bfbd031ffe1df23b8cf253b64e246
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-17 22:10:34 +0800

    roadmap section is done

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,6 +1,8 @@
 import Heading from "./Heading";
 import Section from "./Section";
 import { roadmap } from "../constants";
+import { check2, grid, loading1 } from "../assets";
+import Tagline from "./Tagline";
 
 const Roadmap = () => {
   return (
@@ -14,7 +16,52 @@ const Roadmap = () => {
 
           return (
             <>
-              <div></div>
+              <div
+                key={item.id}
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                  item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                }`}
+              >
+                <div className="relative p-8 bg-n-8 rounded-[2rem] overflow-hidden xl:p-15">
+                  <div className="abosolute top-0 left-0 max-w-full">
+                    <img
+                      src={grid}
+                      alt="Grid"
+                      width={550}
+                      height={550}
+                      className="w-full"
+                    />
+
+                    <div className="relative z-1">
+                      <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
+                        <Tagline>{item.date}</Tagline>
+
+                        <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-6">
+                          <img
+                            src={item.status === "done" ? check2 : loading1}
+                            alt={status}
+                            className="mr-2.5"
+                            width={16}
+                            height={16}
+                          />
+                          <div className="tagline">{status}</div>
+                        </div>
+
+                        <div className="mb-10 -my-10 -mx-15">
+                          <img
+                            src={item.imageUrl}
+                            alt={item.title}
+                            width={630}
+                            height={420}
+                          />
+                        </div>
+                        <h4 className="h4 mb-4">{item.title}</h4>
+                        <p className="body-2 text-n-4">{item.text}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </>
           );
         })}
